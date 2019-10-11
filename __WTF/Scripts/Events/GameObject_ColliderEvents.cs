2cb5bad9fd5574d5087ca9e560e48c7f67402378
Chad Kilgore <ckilgore@gmail.com>
2019-10-10 21:51:19 -0500

    * Added ColliderEvents and UpdateEvent listener MonoBehaviour

---
+++ __WTF/Scripts/Events/GameObject_ColliderEvents.cs
@@ -0,0 +1,62 @@
+﻿namespace UnityEngine
+{
+	public delegate void CollisionEventHandler(object sender, CollisionEventArgs e);
+
+	[Ludiq.IncludeInSettings(true)]
+	public class CollisionEventArgs : System.EventArgs
+	{
+		#region Variables
+
+		private readonly Collision m_collision;
+
+		#endregion
+
+		#region Properties
+
+		public Collider collider => m_collision.collider;
+		public ContactPoint[] contacts => m_collision.contacts;
+		public Vector3 impulse => m_collision.impulse;
+		public Vector3 relativeVelocity => m_collision.relativeVelocity;
+
+		#endregion
+
+		#region Constructors
+
+		public CollisionEventArgs(Collision collision)
+		{
+			m_collision = collision;
+		}
+
+		#endregion
+	}
+
+	public class GameObject_ColliderEvents : MonoBehaviour
+	{
+		#region Events
+
+		public event CollisionEventHandler CollisionEnter;
+		public event CollisionEventHandler CollisionStay;
+		public event CollisionEventHandler CollisionExit;
+
+		#endregion
+
+		#region Methods
+
+		private void OnCollisionEnter(Collision collision)
+		{
+			CollisionEnter?.Invoke(this, new CollisionEventArgs(collision));
+		}
+
+		private void OnCollisionStay(Collision collision)
+		{
+			CollisionStay?.Invoke(this, new CollisionEventArgs(collision));
+		}
+
+		private void OnCollisionExit(Collision collision)
+		{
+			CollisionExit?.Invoke(this, new CollisionEventArgs(collision));
+		}
+
+		#endregion
+	}
+}
---
+++ __WTF/Scripts/Events/GameObject_UpdateEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace UnityEngine
+{
+	public class GameObject_UpdateEvent : MonoBehaviour
+	{
+		#region Events
+
+		public event System.EventHandler OnUpdate;
+
+		#endregion
+
+		#region Methods
+
+		private void Update()
+		{
+			OnUpdate?.Invoke(this, System.EventArgs.Empty);
+		}
+
+		#endregion
+	}
+}
