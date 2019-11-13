f18e7e8acde877ab2dde8c479064e7f49d29758e
Chad Kilgore <ckilgore@gmail.com>
2019-11-12 23:28:52 -0600

    Added events to Health interface

--- __WTF/Scripts/Weapons/IHealth.cs
+++ __WTF/Scripts/Weapons/IHealth.cs
@@ -2,6 +2,14 @@
 {
 	public interface IHealth
 	{
+		event System.EventHandler ValueChanged;
+		event HealthEventHandler Healing;
+		event HealthEventHandler Healed;
+		event HealthEventHandler Hit;
+		event HealthEventHandler Damaging;
+		event HealthEventHandler Damaged;
+		event HealthEventHandler Killed;
+
 		GameObject gameObject { get; }
 
 		void Heal(HealthEventArgs e);
