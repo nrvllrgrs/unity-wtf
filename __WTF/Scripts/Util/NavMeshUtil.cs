57f49180e4dbdf8fb2b6d00a12302914f19a2760
Chad Kilgore <ckilgore@gmail.com>
2020-01-17 23:44:01 -0600

    Additional NavMesh functionality dealing with agentTypes

---
+++ __WTF/Scripts/Util/NavMeshUtil.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using UnityEngine.AI;
+
+namespace UnityEngine.Workshop
+{
+	public static class NavMeshUtil
+	{
+		#region Variables
+
+		private static Dictionary<string, int> s_agentTypeMap;
+
+		#endregion
+
+		#region Methods
+
+		public static string[] GetAgentTypeNames()
+		{
+			int count = NavMesh.GetSettingsCount();
+			var agentTypeNames = new string[count + 2];
+
+			for (int i = 0; i < count; ++i)
+			{
+				int id = NavMesh.GetSettingsByIndex(i).agentTypeID;
+				string name = NavMesh.GetSettingsNameFromID(id);
+				agentTypeNames[i] = name;
+			}
+
+			return agentTypeNames;
+		}
+
+		public static int GetAgentTypeId(string agentType, bool forceReset = false)
+		{
+			if (string.IsNullOrWhiteSpace(agentType))
+				return 0;
+
+			if (forceReset || s_agentTypeMap == null)
+			{
+				ResetAgentTypeIds();
+			}
+
+			return s_agentTypeMap.TryGetValue(agentType, out int value)
+				? value
+				: 0;
+		}
+
+		public static void ResetAgentTypeIds()
+		{
+			s_agentTypeMap = new Dictionary<string, int>();
+
+			int count = NavMesh.GetSettingsCount();
+			for (int i = 0; i < count; ++i)
+			{
+				int id = NavMesh.GetSettingsByIndex(i).agentTypeID;
+				string name = NavMesh.GetSettingsNameFromID(id);
+				s_agentTypeMap.Add(name, id);
+			}
+		}
+
+		#endregion
+	}
+}
