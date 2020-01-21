66f43c488c020f4ab080ea4ecfeb66e9787c12e6
Chad Kilgore <ckilgore@gmail.com>
2020-01-20 20:24:35 -0600

    Changed EulerAngles to Rotation on TransformInfo

--- __WTF/Scripts/TimedCurve/Graphics/TransformModifier.cs
+++ __WTF/Scripts/TimedCurve/Graphics/TransformModifier.cs
@@ -15,7 +15,7 @@ namespace Groundling
 		public bool updatePosition = true;
 
 		[BoxGroup("Transform Settings")]
-		public bool updateEulerAngles = true;
+		public bool updateRotation = true;
 
 		[BoxGroup("Transform Settings")]
 		public bool updateScale = true;
@@ -81,14 +81,14 @@ namespace Groundling
 			{
 				return new TransformInfo(
 					updatePosition ? Vector3.Lerp(source.position, target.position, value) : source.position,
-					updateEulerAngles ? Vector3.Lerp(source.eulerAngles, target.eulerAngles, value) : source.eulerAngles,
+					updateRotation ? Vector3.Lerp(source.eulerAngles, target.eulerAngles, value) : source.eulerAngles,
 					updateScale ? Vector3.Lerp(source.scale, target.scale, value) : source.scale);
 			}
 			else
 			{
 				var transformInfo  = new TransformInfo(
 					updatePosition ? Vector3.Lerp(source.position, source.position + target.position, value) : source.position,
-					updateEulerAngles ? Vector3.Lerp(source.eulerAngles, source.eulerAngles + target.eulerAngles, value) : source.eulerAngles,
+					updateRotation ? Vector3.Lerp(source.eulerAngles, source.eulerAngles + target.eulerAngles, value) : source.eulerAngles,
 					updateScale ? Vector3.Lerp(source.scale, source.scale + target.scale, value) : source.scale);
 
 				if (m_setSourceOnTimedCurveEnd && Mathf.Approximately(timedCurve.timePercent, 1f))
@@ -107,23 +107,44 @@ namespace Groundling
 	{
 		#region Variables
 
-		public Vector3 position, eulerAngles, scale = Vector3.one;
+		[SerializeField]
+		private Vector3 m_position;
+
+		[SerializeField, LabelText("Rotation")]
+		private Vector3 m_eulerAngles;
+
+		[SerializeField]
+		private Vector3 m_scale = Vector3.one;
+
+		#endregion
+
+		#region Properties
+
+		public Vector3 position { get => m_position; set => m_position = value; }
+		public Vector3 eulerAngles { get => m_eulerAngles; set => m_eulerAngles = value; }
+		public Quaternion rotation { get => Quaternion.Euler(m_eulerAngles); set => m_eulerAngles = value.eulerAngles; }
+		public Vector3 scale { get => m_scale; set => m_scale = value; }
 
 		#endregion
 
 		#region Constructors
 
+		public TransformInfo()
+			: this(Vector3.zero, Vector3.zero, Vector3.one)
+		{ }
+
 		public TransformInfo(Transform transform)
-		{
-			position = transform.position;
-			eulerAngles = transform.eulerAngles;
-			scale = transform.lossyScale;
-		}
+			: this(transform.position, transform.rotation, transform.lossyScale)
+		{ }
 
 		public TransformInfo(Vector3 position, Vector3 eulerAngles, Vector3 scale)
+			: this(position, Quaternion.Euler(eulerAngles), scale)
+		{ }
+
+		public TransformInfo(Vector3 position, Quaternion rotation, Vector3 scale)
 		{
 			this.position = position;
-			this.eulerAngles = eulerAngles;
+			this.rotation = rotation;
 			this.scale = scale;
 		}
 
