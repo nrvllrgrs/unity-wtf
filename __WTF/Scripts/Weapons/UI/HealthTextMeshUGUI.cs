3b19f18c681700e55709bb440e560d5767206b89
Chad Kilgore <ckilgore@gmail.com>
2020-02-06 07:53:33 -0600

    Added HealthTextMeshUGUI component

---
+++ __WTF/Scripts/Weapons/UI/HealthTextMeshUGUI.cs
@@ -0,0 +1,58 @@
+﻿using TMPro;
+using Sirenix.OdinInspector;
+
+namespace UnityEngine.Workshop.UI
+{
+	[RequireComponent(typeof(TextMeshProUGUI))]
+	public class HealthTextMeshUGUI : MonoBehaviour
+	{
+		#region Variables
+
+		[SerializeField, Required]
+		private Health m_health;
+
+		[SerializeField]
+		private string m_format = "{0}/<size=55%>{1}</size>";
+
+		[SerializeField]
+		private string m_numericFormat = "#,##0";
+
+		private TextMeshProUGUI m_textMesh;
+
+		#endregion
+
+		#region Properties
+
+		public TextMeshProUGUI textMesh => this.GetComponent(ref m_textMesh);
+
+		#endregion
+
+		#region Methods
+
+		private void OnEnable()
+		{
+			m_health.ValueChanged += ValueChanged;
+		}
+
+		private void OnDisable()
+		{
+			m_health.ValueChanged -= ValueChanged;
+		}
+
+		private void ValueChanged(object sender, System.EventArgs e)
+		{
+			textMesh.text = string.Format(m_format, m_health.value.ToString(m_numericFormat), m_health.maxHealth.ToString(m_numericFormat));
+		}
+
+		private void OnValidate()
+		{
+			try
+			{
+				ValueChanged(this, System.EventArgs.Empty);
+			}
+			catch { }
+		}
+
+		#endregion
+	}
+}
