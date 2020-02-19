6b434a258839cda1466dabcbb622f8d48ec69a15
Chad Kilgore <ckilgore@gmail.com>
2020-02-18 20:07:18 -0600

    Added destroyEvent

---
+++ __WTF/Scripts/Events/GameObject_DestroyEvent.cs
@@ -0,0 +1,20 @@
+﻿namespace UnityEngine
+{
+	public class GameObejct_DestroyEvent : MonoBehaviour
+	{
+		#region Events
+
+		public event System.EventHandler Destroyed;
+
+		#endregion
+
+		#region Methods
+
+		private void OnDestroy()
+		{
+			Destroyed?.Invoke(this, System.EventArgs.Empty);
+		}
+
+		#endregion
+	}
+}
