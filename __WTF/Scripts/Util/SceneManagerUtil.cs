0076ef60b9a539c6d8b9ffb385730c1d05a96d51
Chad Kilgore <ckilgore@gmail.com>
2020-03-16 16:58:10 -0500

    Is loaded scene check

---
+++ __WTF/Scripts/Util/SceneManagerUtil.cs
@@ -0,0 +1,26 @@
+﻿namespace UnityEngine.SceneManagement
+{
+	public static class SceneManagerUtil
+	{
+		#region Methods
+
+		public static bool IsLoaded(Scene scene)
+		{
+			return IsLoaded(scene.name);
+		}
+
+		public static bool IsLoaded(string sceneName)
+		{
+			for (int i = 0; i < SceneManager.sceneCount; ++i)
+			{
+				Scene loadedScene = SceneManager.GetSceneAt(i);
+				if (loadedScene.name == sceneName)
+					return true;
+			}
+
+			return false;
+		}
+
+		#endregion
+	}
+}
