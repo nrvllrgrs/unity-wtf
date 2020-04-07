2a53b6bdc5a6135c32b5b0df7e9d9d3be7062203
Chad Kilgore <ckilgore@gmail.com>
2020-04-06 22:23:38 -0500

    Camera util to cache main camera

---
+++ __WTF/Scripts/Util/CameraUtil.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public static class CameraUtil
+{
+	#region Variables
+
+	private static Camera m_main;
+
+	#endregion
+
+	#region Properties
+
+	public static Camera main
+	{
+		get
+		{
+			if (m_main == null)
+			{
+				m_main = Camera.main;
+			}
+			return m_main;
+		}
+	}
+
+	#endregion
+}
