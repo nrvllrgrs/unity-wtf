2a3af642ab87cd208662d7619e81162f68c6e48a
Chad Kilgore <ckilgore@gmail.com>
2020-05-24 19:21:18 -0500

    PhysicsUtil to ignoreCollision by gameObject

---
+++ __WTF/Scripts/Util/PhysicsUtil.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public static class PhysicsUtil
+{
+	#region Methods
+
+	public static void IgnoreCollision(GameObject obj, GameObject otherObj, bool ignore)
+	{
+		var colliders = obj.GetComponentsInChildren<Collider>(true);
+		var otherColliders = otherObj.GetComponentsInChildren<Collider>(true);
+
+		foreach (var collider in colliders)
+		{
+			foreach (var otherCollider in otherColliders)
+			{
+				Physics.IgnoreCollision(collider, otherCollider, ignore);
+			}
+		}
+	}
+
+	#endregion
+}
